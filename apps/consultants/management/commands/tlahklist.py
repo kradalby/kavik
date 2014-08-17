e60c005b5e783990bc7f19c77cc14da121d9c3e9
Kristoffer Dalby <kradalby@kradalby.no>
2014-08-17 17:44:38 +0200

    added command to output ahk list

---
+++ apps/consultants/management/commands/tlahklist.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand, CommandError
+from apps.consultants.models import Consultant
+from kavik.settings import BASE_DIR
+import os
+
+class Command(BaseCommand):
+    help = "Outputs the teamleader numbers formated as ahk list"
+
+
+    def handle(self, *args, **options):
+        tl = Consultant.objects.filter(number__endswith='01')
+        string = "Teams = "
+        for t in tl:
+            string += t.number[:2] + " "
+        print(string)
+        
